fe6378dcbb5ecb83f5fbf0387237295976773e54
AndradaLuca <37205125+AndradaLuca@users.noreply.github.com>
2018-07-23 10:12:29 +0300

    initial commit

--- SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/MainActivity.java
+++ SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.marius.sportivebets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
