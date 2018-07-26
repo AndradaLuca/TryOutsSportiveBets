ecef52e23931b92f0fdd8ca22d705decb6fb0db8
Marius <pascalaumarius3@gmail.com>
2018-07-26 14:02:11 +0300

    loginviewmodel

--- SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/login/LoginActivity.java
+++ SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/login/LoginActivity.java
@@ -1,42 +1,66 @@
 package com.example.marius.sportivebets.login;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.marius.sportivebets.R;
+import com.example.marius.sportivebets.database.entity.User;
 import com.example.marius.sportivebets.databinding.ActivityLoginBinding;
 import com.example.marius.sportivebets.forgotPassword.ForgotPasswordActivity;
 import com.example.marius.sportivebets.register.RegisterActivity;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class LoginActivity extends AppCompatActivity implements ILoginActivity {
 
-    ActivityLoginBinding loginBinding;
+    private ActivityLoginBinding loginBinding;
+    private LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
-        loginBinding.setILoginActivity((ILoginActivity)this);
+        loginBinding.setILoginActivity(this);
+        initViewModel();
+    }
+
+    private void initViewModel(){
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        loginViewModel.getLoginSuccess().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                Toast.makeText(LoginActivity.this,"login sucses", LENGTH_SHORT).show();
+            }
+        });
+        loginViewModel.getLoginFaild().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                Toast.makeText(LoginActivity.this,s+" bla bla", LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void showRegisterActivity() {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
-
     }
 
     @Override
     public void login() {
-        Toast.makeText(this, "Login Pressed", Toast.LENGTH_SHORT).show();
+        loginViewModel.onLoginClick(loginBinding.loginUsernameEditText.getText().toString(),loginBinding.loginPasswordEditText.getText().toString(),loginBinding.loginSwitch.isChecked());
     }
 
     @Override
     public void forgotPassword() {
-        Toast.makeText(this, "ForgotPassword Pressed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ForgotPassword Pressed", LENGTH_SHORT).show();
         Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
         startActivity(intent);
     }
--- SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/login/LoginViewModel.java
+++ SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/login/LoginViewModel.java
@@ -1,4 +1,36 @@
 package com.example.marius.sportivebets.login;
 
-public class LoginViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.marius.sportivebets.database.entity.User;
+
+public class LoginViewModel extends AndroidViewModel {
+    // corect cu User
+    private MutableLiveData<User> loginSuccess = new MutableLiveData<>();
+    private MutableLiveData<String> loginFaild = new MutableLiveData<>();
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public MutableLiveData<User> getLoginSuccess() {
+        return loginSuccess;
+    }
+
+    public MutableLiveData<String> getLoginFaild() {
+        return loginFaild;
+    }
+
+    public void onLoginClick(String email, String password, boolean isKeepLogged){
+        //todo 1-Validare pe UI 2-
+        // la validare fac un if else si daca ii facuta validare corect
+        if (isKeepLogged) {
+            loginSuccess.postValue(new User());
+        }else
+        loginFaild.postValue("loginFaild");
+       //Log.d("onLoginClick","Email:"+email+" Password:"+password+" isKeepLogged:"+isKeepLogged);
+    }
 }
