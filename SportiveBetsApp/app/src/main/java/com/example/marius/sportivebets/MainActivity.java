c45751f53d666c374b2cf3ecee2f678ac14663bd
Marius <pascalaumarius3@gmail.com>
2018-07-31 13:30:42 +0300

    bottom view

--- SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/MainActivity.java
+++ SportiveBetsApp/app/src/main/java/com/example/marius/sportivebets/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.marius.sportivebets;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.marius.sportivebets.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
+    NavigationView navigationView;
+    ActivityMainBinding mainBinding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
         Bundle bundle = getIntent().getExtras();
         TextView textView = findViewById(R.id.blabla);
         textView.setText(bundle.getString("username"));
